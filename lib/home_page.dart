a17de1539240a456e3a3fabe1bb6279ebd8e1016
netos23 <FromBehindTheWall@users.noreply.github.com>
2020-04-05 16:04:36 +0300

    First launch page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:awsome_text/bar_pages/mic_page.dart';
 import 'package:awsome_text/bar_pages/camera_page.dart';
@@ -5,6 +7,7 @@ import 'package:awsome_text/bar_pages/docgen_page.dart';
 import 'package:awsome_text/bar_pages/pdf_page.dart';
 import 'package:awsome_text/bar_pages/more_page.dart';
 
+import 'package:path_provider/path_provider.dart';
 
 class HomePage extends StatefulWidget{
 
@@ -33,19 +36,60 @@ class HomePageState extends State<HomePage>{
     });
   }
 
+  Future<bool> _isFirstLaunch;
+  bool _isStateChenge;
+  File _initConfig;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFirstLaunch = _getLaunch();
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<bool>(
+      future: _isFirstLaunch,
+      builder: (context,snapshot){
+        return snapshot.hasData
+            ? _isStateChenge
+              ? _buildFirstLaunchPage()
+              : _buildHomePage()
+            : Center(
+              child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
 
+  Future<bool> _getLaunch()async{
+    Directory configPath = await getExternalStorageDirectory();
+    _initConfig = new File(configPath.path+'/config/init.config');
+
+
+    if(_initConfig.existsSync()){
+      _isStateChenge = false;
+      return true;
+    }else{
+      _initConfig.createSync(recursive: true);
+      _isStateChenge = true;
+      return false;
+    }
+  }
+
+
+  Widget _buildHomePage(){
     return Scaffold(
       body: _currentWidget,
       bottomNavigationBar: BottomNavigationBar(
         items: const<BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            backgroundColor: Colors.green,
+              backgroundColor: Colors.green,
               title: Text('Audio notes'),
               icon: Icon(
                 Icons.mic_none,
-                )
+              )
           ),
           BottomNavigationBarItem(
               backgroundColor: Colors.green,
@@ -77,4 +121,189 @@ class HomePageState extends State<HomePage>{
     );
   }
 
+
+  final PageController _controller = PageController(
+
+  );
+
+
+  Widget _buildFirstLaunchPage() {
+    return PageView(
+      controller: _controller,
+      children: <Widget>[
+        Scaffold(
+          //backgroundColor: Colors.white,
+          backgroundColor: Colors.green,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                //Image.asset('images/logo.png'),
+                Image.asset('images/logo_alt.png'),
+                Text(
+                  'Welcome to the Wonderful text app',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      //color: Colors.white,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),Scaffold(
+          backgroundColor: Colors.blue,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                Image.asset('images/camera_app.png'),
+                Text(
+                  'Recognize text from any source in different languages: audio, photos, and images.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.green,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                Image.asset('images/audio_app.png'),
+                Text(
+                  'Create convenient voice notes',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.red,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
+                ),
+                Image.asset('images/pdf_app.png'),
+                Text(
+                  'Create PDF documents from images. Export the recognized text to any format or open it in Google docs',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.green,
+          body: Center(
+            child: Column(
+              children: <Widget>[
+              SizedBox(
+                  height: 50.0,
+                ),
+                Image.asset(
+                    'images/sync.png',
+                    scale: 0.8,
+                ),
+                Text(
+                  'Register or start using the app',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35
+                  ),
+                ),
+                SizedBox(
+                height: 50,
+                ),
+                /*Container(
+                  width: 250,
+                  height: 50,
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(25))
+                  ),
+                  child:FlatButton(
+
+                    onPressed: (){},
+                    //color: Colors.blue,
+                    child: Text(
+                      'SING UP',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30
+                      ),
+                    ),
+                  ) ,
+                ),*/
+                Container(
+                  width: 250,
+                  height: 50,
+
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(25))
+                  ),
+                  child:FlatButton(
+
+                    onPressed: _startApp,
+                    //color: Colors.blue,
+                    child: Text(
+                      'START',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 30
+                      ),
+                    ),
+                  ) ,
+                )
+
+              ],
+            ),
+          ),
+        )
+
+      ],
+    );
+  }
+
+
+  void _startApp() {
+    setState(() {
+      _isStateChenge = false;
+    });
+  }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.green,
+
       ),
       home: HomePage(),
     );
   }
 }
-
-
-
-
-
-
